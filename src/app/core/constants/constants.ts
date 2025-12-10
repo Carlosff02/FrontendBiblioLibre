773a92f51ee414ab096113fcb98b2729acc6305b
Carlosff02 <carlosfelipefdezfalcon@gmail.com>
2025-12-10 18:49:19 -0500

    t

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-  apiUrl:'https://backendbibliolibre.onrender.com//api',
+  apiUrl:'https://6j3lplh2-8080.brs.devtunnels.ms/api',
   CURRENT_TOKEN: 'token',
   CURRENT_USER: 'user',
   ACTUAL_PAGE: 'actual_page'
